9401c9577804eb2eee86f713e9c89a4f33374656
Dell <Dell@DESKTOP-5O23N7P>
2022-07-20 08:17:15 +1030

    [aNKITA]: ADDED MEMEROY1 IN THIS PROJECT

---
+++ src/Love/Memory1.java
@@ -0,0 +1,10 @@
+package Love;
+
+public class Memory1 
+{
+
+	public static void main(String[] args) {
+		
+		System.out.println("I love jk very much you are always my love and my insprision and you are my man and i am your women");
+	}
+}
